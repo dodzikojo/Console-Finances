2545ce91eb92a8f589654660f2b61e9820f90591
dodzikojo <dodzi@windowslive.com>
2022-12-25 07:48:06 +0000

    Completed all key requirements

--- starter/index.js
+++ starter/index.js
@@ -1,105 +1,106 @@
-// var finances = [
-// ['Jan-2010', 867884],
-// ['Feb-2010', 984655],
-// ['Mar-2010', 322013],
-// ['Apr-2010', -69417],
-// ['May-2010', 310503],
-// ['Jun-2010', 522857],
-// ['Jul-2010', 1033096],
-// ['Aug-2010', 604885],
-// ['Sep-2010', -216386],
-// ['Oct-2010', 477532],
-// ['Nov-2010', 893810],
-// ['Dec-2010', -80353],
-// ['Jan-2011', 779806],
-// ['Feb-2011', -335203],
-// ['Mar-2011', 697845],
-// ['Apr-2011', 793163],
-// ['May-2011', 485070],
-// ['Jun-2011', 584122],
-// ['Jul-2011', 62729],
-// ['Aug-2011', 668179],
-// ['Sep-2011', 899906],
-// ['Oct-2011', 834719],
-// ['Nov-2011', 132003],
-// ['Dec-2011', 309978],
-// ['Jan-2012', -755566],
-// ['Feb-2012', 1170593],
-// ['Mar-2012', 252788],
-// ['Apr-2012', 1151518],
-// ['May-2012', 817256],
-// ['Jun-2012', 570757],
-// ['Jul-2012', 506702],
-// ['Aug-2012', -1022534],
-// ['Sep-2012', 475062],
-// ['Oct-2012', 779976],
-// ['Nov-2012', 144175],
-// ['Dec-2012', 542494],
-// ['Jan-2013', 359333],
-// ['Feb-2013', 321469],
-// ['Mar-2013', 67780],
-// ['Apr-2013', 471435],
-// ['May-2013', 565603],
-// ['Jun-2013', 872480],
-// ['Jul-2013', 789480],
-// ['Aug-2013', 999942],
-// ['Sep-2013', -1196225],
-// ['Oct-2013', 268997],
-// ['Nov-2013', -687986],
-// ['Dec-2013', 1150461],
-// ['Jan-2014', 682458],
-// ['Feb-2014', 617856],
-// ['Mar-2014', 824098],
-// ['Apr-2014', 581943],
-// ['May-2014', 132864],
-// ['Jun-2014', 448062],
-// ['Jul-2014', 689161],
-// ['Aug-2014', 800701],
-// ['Sep-2014', 1166643],
-// ['Oct-2014', 947333],
-// ['Nov-2014', 578668],
-// ['Dec-2014', 988505],
-// ['Jan-2015', 1139715],
-// ['Feb-2015', 1029471],
-// ['Mar-2015', 687533],
-// ['Apr-2015', -524626],
-// ['May-2015', 158620],
-// ['Jun-2015', 87795],
-// ['Jul-2015', 423389],
-// ['Aug-2015', 840723],
-// ['Sep-2015', 568529],
-// ['Oct-2015', 332067],
-// ['Nov-2015', 989499],
-// ['Dec-2015', 778237],
-// ['Jan-2016', 650000],
-// ['Feb-2016', -1100387],
-// ['Mar-2016', -174946],
-// ['Apr-2016', 757143],
-// ['May-2016', 445709],
-// ['Jun-2016', 712961],
-// ['Jul-2016', -1163797],
-// ['Aug-2016', 569899],
-// ['Sep-2016', 768450],
-// ['Oct-2016', 102685],
-// ['Nov-2016', 795914],
-// ['Dec-2016', 60988],
-// ['Jan-2017', 138230],
-// ['Feb-2017', 671099]
-// ];
+var finances = [
+['Jan-2010', 867884],
+['Feb-2010', 984655],
+['Mar-2010', 322013],
+['Apr-2010', -69417],
+['May-2010', 310503],
+['Jun-2010', 522857],
+['Jul-2010', 1033096],
+['Aug-2010', 604885],
+['Sep-2010', -216386],
+['Oct-2010', 477532],
+['Nov-2010', 893810],
+['Dec-2010', -80353],
+['Jan-2011', 779806],
+['Feb-2011', -335203],
+['Mar-2011', 697845],
+['Apr-2011', 793163],
+['May-2011', 485070],
+['Jun-2011', 584122],
+['Jul-2011', 62729],
+['Aug-2011', 668179],
+['Sep-2011', 899906],
+['Oct-2011', 834719],
+['Nov-2011', 132003],
+['Dec-2011', 309978],
+['Jan-2012', -755566],
+['Feb-2012', 1170593],
+['Mar-2012', 252788],
+['Apr-2012', 1151518],
+['May-2012', 817256],
+['Jun-2012', 570757],
+['Jul-2012', 506702],
+['Aug-2012', -1022534],
+['Sep-2012', 475062],
+['Oct-2012', 779976],
+['Nov-2012', 144175],
+['Dec-2012', 542494],
+['Jan-2013', 359333],
+['Feb-2013', 321469],
+['Mar-2013', 67780],
+['Apr-2013', 471435],
+['May-2013', 565603],
+['Jun-2013', 872480],
+['Jul-2013', 789480],
+['Aug-2013', 999942],
+['Sep-2013', -1196225],
+['Oct-2013', 268997],
+['Nov-2013', -687986],
+['Dec-2013', 1150461],
+['Jan-2014', 682458],
+['Feb-2014', 617856],
+['Mar-2014', 824098],
+['Apr-2014', 581943],
+['May-2014', 132864],
+['Jun-2014', 448062],
+['Jul-2014', 689161],
+['Aug-2014', 800701],
+['Sep-2014', 1166643],
+['Oct-2014', 947333],
+['Nov-2014', 578668],
+['Dec-2014', 988505],
+['Jan-2015', 1139715],
+['Feb-2015', 1029471],
+['Mar-2015', 687533],
+['Apr-2015', -524626],
+['May-2015', 158620],
+['Jun-2015', 87795],
+['Jul-2015', 423389],
+['Aug-2015', 840723],
+['Sep-2015', 568529],
+['Oct-2015', 332067],
+['Nov-2015', 989499],
+['Dec-2015', 778237],
+['Jan-2016', 650000],
+['Feb-2016', -1100387],
+['Mar-2016', -174946],
+['Apr-2016', 757143],
+['May-2016', 445709],
+['Jun-2016', 712961],
+['Jul-2016', -1163797],
+['Aug-2016', 569899],
+['Sep-2016', 768450],
+['Oct-2016', 102685],
+['Nov-2016', 795914],
+['Dec-2016', 60988],
+['Jan-2017', 138230],
+['Feb-2017', 671099]
+];
 
 //Test Values
-var finances = [
-    ['Jan-2010', 69],
-    ['Feb-2010', 100],
-    ['Mar-2010', 80]
-    
-    ];
+// var finances = [
+//     ['Jan-2010', 69],
+//     ['Feb-2010', 100],
+//     ['Mar-2010', 170]
+//     ];
     
 
-//All Variable
+//All Variables
+
+var loopCounter = 0;
+
 var totalMonths = 0;
 var totalValue = 0;
-var averageChange = 0;
 
 var greatestProfitIncrease = "";
 var greatestProfitIncreaseMonthYear = "";
@@ -107,9 +108,11 @@ var greatestProfitIncreaseMonthYear = "";
 var greatestProfitDecrease = "";
 var greatestProfitDecreaseMonthYear = "";
 
-var currentValue = 0;
 var higherValue = 0;
-var totalChange = 0;
+var higherValueMonthYear = "";
+
+var lowerValue = 0;
+var lowerValueMonthYear = "";
 
 var previousMonthValue = 0;
 var previousMonthYear = "";
@@ -121,43 +124,82 @@ var currentChangeValue = 0;
 var previousChangeValue = 0;
 var totalChangeValue = 0;
 
-finances.forEach(month => {
-    console.log("Current Month Value is: "+month[1] );
-    console.log("Previous Value is: "+previousMonthValue);
-    console.log("Previous Change Value: "+previousChangeValue);
-    console.log("---------------------");
 
+finances.forEach(month => {
+    
+    
     currentMonthYear = month[0];
     currentMonthValue = month[1];
 
-    if (previousMonthValue > 0) {
-        currentChangeValue = previousMonthValue - currentMonthValue;
+    currentChangeValue =   currentMonthValue - previousMonthValue;
+    
+    console.log("---------------------"+(loopCounter+1)+"---------------------");
+    console.log("Current Month is: "+  currentMonthYear);
+    console.log("Previous Month Value is: $"+previousMonthValue);
+    console.log("Current Month Value is: $"+currentMonthValue );
+    console.log("Change Value: $"+currentChangeValue);
+    
+
+    if (loopCounter > 0) {
+
         totalChangeValue += currentChangeValue; 
+
+        //Code for checking the greatest profit increase
+        if (currentChangeValue >= previousChangeValue) {
+            higherValue = currentMonthValue;
+            higherValueMonthYear = currentMonthYear;
+
+            greatestProfitIncreaseValue =   currentChangeValue;
+            console.log("Current is greater :$"+higherValue);
+        }
+        else{
+            console.log("Previous is greater :$"+higherValue);
+        }
+
+        //Code for checking the greatest profit decrease
+        if (currentChangeValue <= previousChangeValue) {
+            lowerValue = currentMonthValue;
+            lowerValueMonthYear = currentMonthYear;
+
+            greatestProfitDecreaseValue = currentChangeValue;
+            console.log("Current is lower :$"+lowerValue);
+        }
+        else {
+            lowerValue = previousMonthValue;
+            lowerValueMonthYear = previousMonthYear;
+
+            console.log("Previous is lower :$"+lowerValue);
+        }
     }
 
-    if (currentChangeValue > previousChangeValue) {
-        greatestProfitIncrease = currentMonthValue;
-        greatestProfitIncreaseMonthYear = currentMonthYear;
-    }
-    else {
-        greatestProfitIncrease = previousMonthValue;
-        greatestProfitIncreaseMonthYear = previousMonthYear;
-    }
+    
+
+    greatestProfitIncrease = higherValue;
+    greatestProfitIncreaseMonthYear = higherValueMonthYear;
+
+    
+    greatestProfitDecrease = lowerValue;
+    greatestProfitDecreaseMonthYear = lowerValueMonthYear;
 
     previousMonthValue = currentMonthValue;
     previousMonthYear = currentMonthYear;
-    previousChangeValue = currentChangeValue;
+
+    if (loopCounter > 0) {
+        previousChangeValue = currentChangeValue;
+    }
+    
 
 
     totalMonths++;
     totalValue+=month[1];
+
+    loopCounter++;
 });
 
 
-
-
+console.log("\n");
 console.log("Financial Analysis");
-console.log("----------------------------------------------");
+console.log("----------------------------");
 
 // The total number of months included in the dataset.
 console.log("Total Months: "+ totalMonths);
@@ -166,10 +208,10 @@ console.log("Total Months: "+ totalMonths);
 console.log("Total: $"+ totalValue);
 
 // The average of the changes in Profit/Losses over the entire period.
-console.log("Average Change: $"+ totalChangeValue/totalMonths);
+console.log("Average Change: $"+ (totalChangeValue/totalMonths).toFixed(2));
 
 //The greatest increase in profits (date and amount) over the entire period.
 console.log("Greatest Increase in Profits: "+greatestProfitIncreaseMonthYear +" ($"+greatestProfitIncrease+")");
 
 // The greatest decrease in losses (date and amount) over the entire period.
-console.log("Greatest Decrease in Profits: $");
+console.log("Greatest Decrease in Profits: "+greatestProfitDecreaseMonthYear +" ($"+greatestProfitDecrease+")");
